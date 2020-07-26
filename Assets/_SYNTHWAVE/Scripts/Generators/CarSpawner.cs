23dc587d9d48b3c2e212ca0fab6e0271e25a5289
Jamie <jmb1g17@soton.ac.uk>
2020-07-26 20:22:00 +0100

    Added new car

--- Assets/_SYNTHWAVE/Scripts/Generators/CarSpawner.cs
+++ Assets/_SYNTHWAVE/Scripts/Generators/CarSpawner.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 
 public class CarSpawner : MonoBehaviour
 {
-	[SerializeField] private List<GameObject> _cars;
-	[SerializeField] private int _carIndex;
+	[SerializeField] private List<GameObject> _cars;	
 	[SerializeField] private GameObject _carParent;
+
+	private int _carIndex;
 
 	private void Awake()
 	{
-		SpawnCar();
+		SpawnCar(Random.Range(0, _cars.Count +1));
 	}
 
-	private void SpawnCar()
+	private void SpawnCar(int index)
 	{
 		GameObject currentCar;
-		currentCar = Instantiate(_cars[_carIndex]);
+		currentCar = Instantiate(_cars[index]);
 
 		currentCar.transform.SetParent(_carParent.transform);
 		currentCar.transform.localPosition = Vector3.zero;
